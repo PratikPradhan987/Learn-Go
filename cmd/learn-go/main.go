9c6d8840245b112f4afb96709a820ba1269ee4e0
Pratik <pp3757634@gmail.com>
2024-10-06 22:00:51 +0530

    graceful shutdown

--- cmd/learn-go/main.go
+++ cmd/learn-go/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/PratikPradhan987/learn-go/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	cfg := config.MustLoad()
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request){
+        w.Write([]byte("Hello, World!"))
+	})
+
+	server := http.Server {
+		Addr:  cfg.Addr,
+        Handler: router,
+	}
+	slog.Info("Server Started",slog.String("address" ,cfg.Addr))
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func(){
+		err := server.ListenAndServe()
+
+		if err!= nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
+	<-done
+	slog.Info("Shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err!= nil {
+		slog.Error("Shutting down the server",slog.String("error" ,err.Error()))
+    }
+
+	slog.Info("Server gracefully stopped")
 }
